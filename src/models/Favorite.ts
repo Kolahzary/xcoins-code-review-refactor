881f13996c577928e1423586cb7f8f630dbba723
New <shadman.k@defraged.com>
2021-12-17 13:06:29 +0330

    BREAKING CHANGE: refactor favorites model

--- src/models/Favorite.ts
+++ src/models/Favorite.ts
@@ -6,9 +6,9 @@ const schema = new Schema(
   {
     profile_id: String,
     name: String,
-    favorite1: String,
-    favorite2: String,
-    favorite3: String,
+    favorites: [{
+      type: String
+    }]
   },
   {
     timestamps: true,
